80fc01bd66d6c2909879cab68515fac4f42272f3
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2014-03-20 09:23:21 +0700

    Added model validation rules ref : http://laravel.com/docs/validation

--- app/models/Jobprefix.php
+++ app/models/Jobprefix.php
@@ -7,4 +7,18 @@ class Jobprefix extends Morpaworks\Database\Model {
      * @var array
      */
     protected $fillable = ['code', 'title'];
+
+    /**
+     * Model validation rules
+     * ref : app\Morphaworks\Database\Model.php
+     *       http://laravel.com/docs/validation
+     * @var array
+     */
+    protected static $rules = array(
+        // Validation rule: code field is required and must unique in jobprefixes
+        // table in column code. But ignore code value for given id (for update)
+        'code' => 'required|unique:jobprefixes,code,:id',
+        'title' => 'required|unique:jobprefixes,title,:id',
+    );
+
 }
