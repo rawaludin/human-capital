77ce78544d58d768510bdd458bb2125b0acc06b7
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2014-03-21 05:05:44 +0700

    Added jobtitles migration

---
+++ app/database/migrations/2014_03_20_220038_create_jobtitles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateJobtitlesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('jobtitles', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('jobprefix_id')->unsigned();
+			$table->integer('functionalscope_id')->unsigned();
+			$table->string('title')->unique();
+			$table->foreign('functionalscope_id')->references('id')->on('functionalscopes')
+				  ->onUpdate('cascade')->onDelete('restrict');
+			$table->foreign('jobprefix_id')->references('id')->on('jobprefixes')
+				  ->onUpdate('cascade')->onDelete('restrict');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('jobtitles');
+	}
+
+}
