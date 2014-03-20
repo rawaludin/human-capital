f201390d1d670c29fd8abc42cfacc1d847151ca2
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2014-03-20 09:11:16 +0700

    Added mass asignment rule for jobprefix model ref : http://laravel.com/docs/eloquent#mass-assignment

--- app/models/Jobprefix.php
+++ app/models/Jobprefix.php
@@ -1,5 +1,10 @@
 <?php
 
 class Jobprefix extends Morpaworks\Database\Model {
-
+    /**
+     * white list column for mass asignment
+     * ref : http://laravel.com/docs/eloquent#mass-assignment
+     * @var array
+     */
+    protected $fillable = ['code', 'title'];
 }
